b19d042809bd72e92e1076b15838d9aaf2151bbb
joeljojo <joelpamphyl@gmail.com>
2022-09-28 12:43:11 +0300

    Addes nasa astronomy picture of the day

---
+++ src/Components/Header/PictureOfTheDay.jsx
@@ -0,0 +1,40 @@
+import React, {useState, useEffect} from 'react';
+import Axios from 'axios';
+import { NASA_PIC_OF_THE_DAY_URL, NASA_API_KEY } from '../../Config/Config.js';
+import '../Header/header.css';
+
+//function to find the day of the week
+function dayOfTheWeek() {
+    const options = {day: 'numeric', weekday:'long', month: 'long', year:'numeric'}
+    const day = new Date().toLocaleDateString('en-us', options);
+    return day;
+}
+
+const PictureOfTheDay = () => {
+    const [picture, setPicture] = useState([]);
+useEffect(() => {
+// Fetching the Astronomy picture of the day
+    Axios.get(NASA_PIC_OF_THE_DAY_URL + NASA_API_KEY).then((response) =>{
+        setPicture(response.data);
+
+    });
+    
+},[])
+    
+    return (
+        <div>
+            <div className='header'>
+
+                <div className="title">
+                    <h1><span>NASA: </span>Picture of the Day
+                        <div>{dayOfTheWeek()}</div>
+                    </h1>
+                </div>
+                <div className="pic"><img src={picture.hdurl} alt="NASA Picture of the Day" layout='responsive'/></div>
+            
+            </div>
+        </div>)
+        
+}
+
+export default PictureOfTheDay;
---
+++ src/Config/Config.js
@@ -0,0 +1,10 @@
+// Application configuration URLs
+const NASA_API_KEY = 'tFjpg7n8seCEgCDBQnto3P9PnHvc7whtgA0PEgFE';
+const NASA_PIC_OF_THE_DAY_URL = 'https://api.nasa.gov/planetary/apod?api_key=';
+const IMDB_API_KEY = '48b43c71c226d58239efb833d05ab17c&language=en-US&query=NASA&include_adult=false&1';
+const IMDB_API_URL='https://api.themoviedb.org/3/search/movie?api_key=';
+const POSTER_URL = 'https://image.tmdb.org/t/p/original//'
+
+export {NASA_API_KEY,IMDB_API_KEY,NASA_PIC_OF_THE_DAY_URL, IMDB_API_URL,POSTER_URL};
+
+
